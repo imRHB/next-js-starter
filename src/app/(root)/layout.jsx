bf6063aaeba2d82764a4e049847087b4b4d0f8d1
Rakib Hasan Babu <rhbabu3@gmail.com>
2024-06-06 21:30:28 +0600

    typo :pencil2: type

--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Footer from "@/components/Footer";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
     title: "Next JS Starter",
